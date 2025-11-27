97c4329c5f14d6d377971ac2e5682fdf95f2de2f
Constantin George <75323056+gorgo-cpu@users.noreply.github.com>
2025-11-27 18:58:02 +0200

    Fix base path in Vite configuration

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/TutoringDE", // <--- IMPORTANT: Change REPO_NAME to your actual GitHub repository name
+  base: "/TutoringDE/", 
 })
